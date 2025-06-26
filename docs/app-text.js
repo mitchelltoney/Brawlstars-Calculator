c72dbdd55a6819f7a08320c8c163779dd44f9dc8
Mitchell Toney <125512960+mitchelltoney@users.noreply.github.com>
2025-06-26 16:23:37 -0700

    refactor: share brawler list

--- docs/app-text.js
+++ docs/app-text.js
@@ -1,14 +1,5 @@
 
-const brawlerNames = [
-    "8bit","Amber","Angelo","Ash","Barley","Bea","Belle","Berry","Bibi","Bo","Bonnie","Brock",
-    "Bull","Buster","Buzz","Byron","Carl","Charlie","Chester","Chuck","Clancy","Colette","Colt",
-    "Cordelius","Crow","Darryl","Doug","Draco","Dynamike","Edgar","El Primo","Emz","Eve","Fang","Finx",
-    "Frank","Gale","Gene","Gray","Griff","Grom","Gus","Hank","Jacky","Jae Young","Janet","Jessie","Juju","Kaze",
-    "Kenji","Kit","Larry","Leon","Lily","Lola","Lou","Lumi","Maisie","Mandy","Max","Meeple","Meg",
-    "Melodie","Mico","Moe","Mortis","Mr P","Nani","Nita","Ollie", "Otis","Pam","Pearl","Penny","Piper",
-    "Poco","RT","Rico","Rosa","Ruffs","Sam","Sandy","Shade","Shelly","Spike","Sprout","Squeak",
-    "Stu","Surge","Tara","Tick","Willow"
-    ];
+import { brawlerNames } from "./brawlers.js";
 /* ---------- helper identical to app.js ---------- */
 function fileName(n){
     return n.toLowerCase().replace(/[\s.']/g,"_") + ".png";
