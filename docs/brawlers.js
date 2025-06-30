e900461227194ba9c6689e49b41166ac291ef360
Mitchell Toney <mitchelltoney@Mitchells-MacBook-Pro-2670.local>
2025-06-30 12:21:55 -0700

    Added rarity brawlerlist and reformatted original

--- docs/brawlers.js
+++ docs/brawlers.js
@@ -1,10 +1,21 @@
 export const brawlerNames = [
-    "8bit","Amber","Angelo","Ash","Barley","Bea","Belle","Berry","Bibi","Bo","Bonnie","Brock",
-    "Bull","Buster","Buzz","Byron","Carl","Charlie","Chester","Chuck","Clancy","Colette","Colt",
-    "Cordelius","Crow","Darryl","Doug","Draco","Dynamike","Edgar","El Primo","Emz","Eve","Fang","Finx",
-    "Frank","Gale","Gene","Gray","Griff","Grom","Gus","Hank","Jacky","Jae Young","Janet","Jessie","Juju","Kaze",
-    "Kenji","Kit","Larry","Leon","Lily","Lola","Lou","Lumi","Maisie","Mandy","Max","Meeple","Meg",
-    "Melodie","Mico","Moe","Mortis","Mr P","Nani","Nita","Ollie","Otis","Pam","Pearl","Penny","Piper",
-    "Poco","RT","Rico","Rosa","Ruffs","Sam","Sandy","Shade","Shelly","Spike","Sprout","Squeak",
-    "Stu","Surge","Tara","Tick","Willow"
+    "8bit", "Amber", "Angelo", "Ash", "Barley", "Bea", "Belle", "Berry", "Bibi", "Bo", "Bonnie", "Brock",
+    "Bull", "Buster", "Buzz", "Byron", "Carl", "Charlie", "Chester", "Chuck", "Clancy", "Colette", "Colt",
+    "Cordelius", "Crow", "Darryl", "Doug", "Draco", "Dynamike", "Edgar", "El Primo", "Emz", "Eve", "Fang",
+    "Finx", "Frank", "Gale", "Gene", "Gray", "Griff", "Grom", "Gus", "Hank", "Jacky", "Jae Young", "Janet", 
+    "Jessie", "Juju", "Kaze", "Kenji", "Kit", "Larry", "Leon", "Lily", "Lola", "Lou", "Lumi", "Maisie", "Mandy",
+    "Max", "Meeple", "Meg", "Melodie", "Mico", "Moe", "Mortis", "Mr P", "Nani", "Nita", "Ollie", "Otis", "Pam",
+    "Pearl", "Penny", "Piper", "Poco", "RT", "Rico", "Rosa", "Ruffs", "Sam", "Sandy", "Shade", "Shelly", "Spike",
+    "Sprout", "Squeak", "Stu", "Surge", "Tara", "Tick", "Willow"
+];
+
+export const brawlerNamesRarity = [
+    "Shelly", "Nita", "Colt", "Bull", "Brock", "El Primo", "Barley", "Poco", "Rosa", "Jessie", "Dynamike",
+    "Tick", "8bit", "Rico", "Darryl", "Penny", "Carl", "Jacky", "Gus", "Bo", "Emz", "Stu", "Piper", "Pam",
+    "Frank", "Bibi", "Bea", "Nani", "Edgar", "Griff", "Grom", "Bonnie", "Gale", "Colette", "Belle", "Ash",
+    "Lola", "Sam", "Mandy", "Maisie", "Hank", "Pearl", "Larry", "Angelo", "Berry", "Shade", "Meeple", "Mortis",
+    "Tara", "Gene", "Max", "Mr P", "Sprout", "Byron", "Squeak", "Lou", "Ruffs", "Buzz", "Fang", "Eve", "Janet",
+    "Otis", "Buster", "Gray", "RT", "Willow", "Doug", "Chuck", "Charlie", "Mico", "Melodie", "Lily", "Clancy",
+    "Moe", "Juju", "Ollie", "Lumi", "Finx", "Jae Young", "Spike", "Crow", "Leon", "Sandy", "Amber", "Meg",
+    "Surge", "Chester", "Cordelius", "Kit", "Draco", "Kenji", "Kaze"
 ];
