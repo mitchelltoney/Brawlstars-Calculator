04d59003663eda01d81c9bbe75a4b0b92818bbb3
Mitchell Toney <125512960+mitchelltoney@users.noreply.github.com>
2025-06-30 14:51:10 -0700

    Add sorting toggle for brawler icons

--- docs/brawlers.js
+++ docs/brawlers.js
@@ -8,3 +8,16 @@ export const brawlerNames = [
     "Poco","RT","Rico","Rosa","Ruffs","Sam","Sandy","Shade","Shelly","Spike","Sprout","Squeak",
     "Stu","Surge","Tara","Tick","Willow"
 ];
+
+// Brawler names ordered by rarity ranking. This currently mirrors the
+// alphabetical list but can be adjusted to reflect actual rarity tiers.
+export const brawlerNamesRarity = [
+    "8bit","Amber","Angelo","Ash","Barley","Bea","Belle","Berry","Bibi","Bo","Bonnie","Brock",
+    "Bull","Buster","Buzz","Byron","Carl","Charlie","Chester","Chuck","Clancy","Colette","Colt",
+    "Cordelius","Crow","Darryl","Doug","Draco","Dynamike","Edgar","El Primo","Emz","Eve","Fang","Finx",
+    "Frank","Gale","Gene","Gray","Griff","Grom","Gus","Hank","Jacky","Jae Young","Janet","Jessie","Juju","Kaze",
+    "Kenji","Kit","Larry","Leon","Lily","Lola","Lou","Lumi","Maisie","Mandy","Max","Meeple","Meg",
+    "Melodie","Mico","Moe","Mortis","Mr P","Nani","Nita","Ollie","Otis","Pam","Pearl","Penny","Piper",
+    "Poco","RT","Rico","Rosa","Ruffs","Sam","Sandy","Shade","Shelly","Spike","Sprout","Squeak",
+    "Stu","Surge","Tara","Tick","Willow"
+];
