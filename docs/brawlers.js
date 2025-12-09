463f41fdb58273b2cf5c5c549f45015da7fb32b7
Mitchell Toney <mitchjt97@gmail.com>
2025-12-09 14:44:03 -0800

    Add new brawlers

--- docs/brawlers.js
+++ docs/brawlers.js
@@ -2,20 +2,20 @@ export const brawlerNames = [
     "8bit", "Alli", "Amber", "Angelo", "Ash", "Barley", "Bea", "Belle", "Berry", "Bibi", "Bo", "Bonnie", "Brock",
     "Bull", "Buster", "Buzz", "Byron", "Carl", "Charlie", "Chester", "Chuck", "Clancy", "Colette", "Colt",
     "Cordelius", "Crow", "Darryl", "Doug", "Draco", "Dynamike", "Edgar", "El Primo", "Emz", "Eve", "Fang",
-    "Finx", "Frank", "Gale", "Gene", "Gray", "Griff", "Grom", "Gus", "Hank", "Jacky", "Jae Yong", "Janet", 
+    "Finx", "Frank", "Gale", "Gene", "Gigi", "Gray", "Griff", "Grom", "Gus", "Hank", "Jacky", "Jae Yong", "Janet", 
     "Jessie", "Juju", "Kaze", "Kenji", "Kit", "Larry", "Leon", "Lily", "Lola", "Lou", "Lumi", "Maisie", "Mandy",
-    "Max", "Meeple", "Meg", "Melodie", "Mico", "Moe", "Mortis", "Mr P", "Nani", "Nita", "Ollie", "Otis", "Pam",
+    "Max", "Meeple", "Meg", "Melodie", "Mico", "Mina", "Moe", "Mortis", "Mr P", "Nani", "Nita", "Ollie", "Otis", "Pam",
     "Pearl", "Penny", "Piper", "Poco", "RT", "Rico", "Rosa", "Ruffs", "Sam", "Sandy", "Shade", "Shelly", "Spike",
-    "Sprout", "Squeak", "Stu", "Surge", "Tara", "Tick", "Willow"
+    "Sprout", "Squeak", "Stu", "Surge", "Tara", "Tick", "Trunk", "Willow", "Ziggy"
 ];
 
 export const brawlerNamesRarity = [
     "Shelly", "Nita", "Colt", "Bull", "Brock", "El Primo", "Barley", "Poco", "Rosa", "Jessie", "Dynamike",
     "Tick", "8bit", "Rico", "Darryl", "Penny", "Carl", "Jacky", "Gus", "Bo", "Emz", "Stu", "Piper", "Pam",
     "Frank", "Bibi", "Bea", "Nani", "Edgar", "Griff", "Grom", "Bonnie", "Gale", "Colette", "Belle", "Ash",
-    "Lola", "Sam", "Mandy", "Maisie", "Hank", "Pearl", "Larry", "Angelo", "Berry", "Shade", "Meeple", "Mortis",
+    "Lola", "Sam", "Mandy", "Maisie", "Hank", "Pearl", "Larry", "Angelo", "Berry", "Shade", "Meeple", "Trunk", "Mortis",
     "Tara", "Gene", "Max", "Mr P", "Sprout", "Byron", "Squeak", "Lou", "Ruffs", "Buzz", "Fang", "Eve", "Janet",
     "Otis", "Buster", "Gray", "RT", "Willow", "Doug", "Chuck", "Charlie", "Mico", "Melodie", "Lily", "Clancy",
-    "Moe", "Juju", "Ollie", "Lumi", "Finx", "Alli", "Jae Yong", "Spike", "Crow", "Leon", "Sandy", "Amber", "Meg",
+    "Moe", "Juju", "Ollie", "Lumi", "Finx", "Mina", "Ziggy", "Alli", "Jae Yong", "Spike", "Crow", "Leon", "Sandy", "Amber", "Meg",
     "Surge", "Chester", "Cordelius", "Kit", "Draco", "Kenji", "Kaze"
 ];
